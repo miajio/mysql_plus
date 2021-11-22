2c73ab1d24c475b2b15a893a0426345429605845
xiaoby <xiaoboyi@allcorehatress.com>
2021-11-22 11:57:34 +0800

    拆离部分逻辑,转换为util中处理

--- init.go
+++ init.go
@@ -2,18 +2,11 @@ package mysqlplus
 
 import (
 	"fmt"
-	"reflect"
 	"strings"
 )
 
 // modelCententInteface 模型处理中心
 type modelCententInteface interface {
-	getColumns(val interface{}) ([]string, []interface{}) // 获取字段数组及设值数组
-
-	getAllTags(val interface{}, tag string) []string // 获取当前结构体对应标签数据
-
-	isBlank(value reflect.Value) bool // 判断值是否为空
-
 	Insert(table string, val interface{}) (string, []interface{}) // Insert 自动生成Insert语句方法
 
 	Update(table string, set interface{}, where string, whereParams ...interface{}) (string, []interface{}) // Update 自动生成Update语句方法
@@ -35,7 +28,7 @@ func (mc *modelCententStruct) Insert(table string, val interface{}) (string, []i
 
 	wheres := make([]string, 0)
 
-	columns, params := mc.getColumns(val)
+	columns, params := Util.GetColumns(val, "db")
 	for i := 0; i < len(columns); i++ {
 		wheres = append(wheres, "?")
 	}
@@ -47,7 +40,7 @@ func (mc *modelCententStruct) Insert(table string, val interface{}) (string, []i
 // Update 自动生成Update语句方法
 func (mc *modelCententStruct) Update(table string, set interface{}, where string, whereParams ...interface{}) (string, []interface{}) {
 	sql := `update %s set %s `
-	columns, resultParams := mc.getColumns(set)
+	columns, resultParams := Util.GetColumns(set, "db")
 	setStr := ""
 	if len(columns) > 1 {
 		setStr = strings.Join(columns, " = ?, ") + " = ?"
@@ -79,68 +72,11 @@ func (mc *modelCententStruct) Delete(table string, where string, whereParams ...
 // Select 自动生成Select语句
 func (mc *modelCententStruct) Select(table, where string, model interface{}, whereParams ...interface{}) (string, []interface{}) {
 	sql := `select %s from %s`
-	columns := strings.Join(mc.getAllTags(model, "db"), ",")
+	columns := strings.Join(Util.GetAllTags(model, "db"), ",")
 	if where != "" {
 		sql = sql + " where %s"
 		return fmt.Sprintf(sql, columns, table, where), whereParams
 	}
 
 	return fmt.Sprintf(sql, columns, table), whereParams
-}
-
-// getColumns 基础内核方法 获取参数数据
-// 仅返回数据非空的字段数据及数据结果
-func (mc *modelCententStruct) getColumns(val interface{}) ([]string, []interface{}) {
-
-	columns := make([]string, 0)
-	params := make([]interface{}, 0)
-
-	typeOf := reflect.TypeOf(val)
-	valueOf := reflect.ValueOf(val)
-
-	for i := 0; i < typeOf.NumField(); i++ {
-		column := typeOf.Field(i).Tag.Get("db")
-		value := valueOf.Field(i).Interface()
-		if !mc.isBlank(reflect.ValueOf(value)) {
-			columns = append(columns, column)
-			params = append(params, value)
-		}
-	}
-
-	return columns, params
-}
-
-// getAllTags 基础内核方法 获取参数标签数据
-// 获取所有字段数据及数据结果
-func (mc *modelCententStruct) getAllTags(val interface{}, tag string) []string {
-
-	columns := make([]string, 0)
-
-	typeOf := reflect.TypeOf(val)
-
-	for i := 0; i < typeOf.NumField(); i++ {
-		column := typeOf.Field(i).Tag.Get("db")
-		columns = append(columns, column)
-	}
-
-	return columns
-}
-
-// isBlank 判断值是否为空
-func (mc *modelCententStruct) isBlank(value reflect.Value) bool {
-	switch value.Kind() {
-	case reflect.String:
-		return value.Len() == 0
-	case reflect.Bool:
-		return !value.Bool()
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		return value.Int() == 0
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-		return value.Uint() == 0
-	case reflect.Float32, reflect.Float64:
-		return value.Float() == 0
-	case reflect.Interface, reflect.Ptr:
-		return value.IsNil()
-	}
-	return reflect.DeepEqual(value.Interface(), reflect.Zero(value.Type()).Interface())
 }
---
+++ util.go
@@ -0,0 +1,92 @@
+package mysqlplus
+
+import (
+	"reflect"
+	"strings"
+
+	"github.com/go-basic/uuid"
+)
+
+type utilInterface interface {
+	GetColumns(val interface{}, tag string) ([]string, []interface{}) // 获取字段数组及设值数组
+
+	GetAllTags(val interface{}, tag string) []string // 获取当前结构体对应标签数据
+
+	IsBlank(value reflect.Value) bool // 判断值是否为空
+
+	UUID() string // 获取UUID
+
+	UUIDReplaceAll(old, new string) string // UUIDReplaceAll 获取UUID并进行替换字符
+
+}
+
+type utilStruct struct{}
+
+var Util utilInterface = (*utilStruct)(nil)
+
+// GetColumns 基础内核方法 获取参数数据
+// 仅返回数据非空的字段数据及数据结果
+func (u *utilStruct) GetColumns(val interface{}, tag string) ([]string, []interface{}) {
+
+	columns := make([]string, 0)
+	params := make([]interface{}, 0)
+
+	typeOf := reflect.TypeOf(val)
+	valueOf := reflect.ValueOf(val)
+
+	for i := 0; i < typeOf.NumField(); i++ {
+		column := typeOf.Field(i).Tag.Get(tag)
+		value := valueOf.Field(i).Interface()
+		if !u.IsBlank(reflect.ValueOf(value)) {
+			columns = append(columns, column)
+			params = append(params, value)
+		}
+	}
+
+	return columns, params
+}
+
+// GetAllTags 基础内核方法 获取参数标签数据
+// 获取所有字段数据及数据结果
+func (u *utilStruct) GetAllTags(val interface{}, tag string) []string {
+
+	columns := make([]string, 0)
+
+	typeOf := reflect.TypeOf(val)
+
+	for i := 0; i < typeOf.NumField(); i++ {
+		column := typeOf.Field(i).Tag.Get("db")
+		columns = append(columns, column)
+	}
+
+	return columns
+}
+
+// IsBlank 判断值是否为空
+func (u *utilStruct) IsBlank(value reflect.Value) bool {
+	switch value.Kind() {
+	case reflect.String:
+		return value.Len() == 0
+	case reflect.Bool:
+		return !value.Bool()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return value.Int() == 0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return value.Uint() == 0
+	case reflect.Float32, reflect.Float64:
+		return value.Float() == 0
+	case reflect.Interface, reflect.Ptr:
+		return value.IsNil()
+	}
+	return reflect.DeepEqual(value.Interface(), reflect.Zero(value.Type()).Interface())
+}
+
+// UUID 获取UUID
+func (u *utilStruct) UUID() string {
+	return uuid.New()
+}
+
+// UUIDReplaceAll 获取UUID并进行替换字符
+func (u *utilStruct) UUIDReplaceAll(old, new string) string {
+	return strings.ReplaceAll(u.UUID(), old, new)
+}
