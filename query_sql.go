ab5958225a4305737bd4e846449ae024c5110d8f
xiaoby <xiaoboyi@allcorehatress.com>
2021-11-23 18:07:27 +0800

    新增join模式查询逻辑

--- query_sql.go
+++ query_sql.go
@@ -9,48 +9,114 @@ type querySQLInterface interface {
 }
 
 type QuerySQL struct {
-	table       string        // 表名
-	model       interface{}   // 内核主表模型
-	params      []interface{} // 实际内核参数
-	sql         string        // 最终sql
-	where       string        // where条件
-	whereParams []interface{} // where条件参数数组
-	set         string        // set语句
-	setParams   []interface{} // set对应参数数组
+	table        string              // 表名
+	model        interface{}         // 内核主表模型
+	params       []interface{}       // 实际内核参数
+	sql          string              // 最终sql
+	where        string              // where条件
+	whereParams  []interface{}       // where条件参数数组
+	set          string              // set语句
+	setParams    []interface{}       // set对应参数数组
+	group        int                 // 类型 1 查询 2 修改 3 删除 4 表关联模式查询
+	queryColumns string              // 查询列数据
+	joinSql      string              // 表关联逻辑sql
+	joinOn       map[string]SqlParam // 表关联逻辑模式 on key 别名 value 表关联关系sql
+	joinParams   []interface{}       // 表关联逻辑模式 参数数组
+	resultModel  interface{}         // 返回数据模型
 }
 
-func (q *QuerySQL) Select() {
+type SqlParam struct {
+	Sql    string        // sql语句
+	Params []interface{} // 对应参数数组
+}
+
+func (q *QuerySQL) Select(queryColumns string) *QuerySQL {
 	c := Util.GetAllTags(q.model, "db")
 	sql := `SELECT %s FROM %s`
-	q.sql = fmt.Sprintf(sql, strings.Join(c, ","), q.table)
+	if queryColumns != "" {
+		q.sql = fmt.Sprintf(sql, queryColumns, q.table)
+	} else {
+		q.sql = fmt.Sprintf(sql, strings.Join(c, ","), q.table)
+	}
+	q.group = 1
+	return q
 }
 
-func (q *QuerySQL) Update() {
+func (q *QuerySQL) SelectJoin(queryColumns string) *QuerySQL {
+	q = q.Select(queryColumns)
+	q.group = 4
+	return q
+}
+
+// Join 关联查询逻辑
+// joinType 0 cross join 1 inner join 2 left join 3 right join
+func (q *QuerySQL) Join(joinType int, joinOn map[string]SqlParam) *QuerySQL {
+
+	joinSql := ""
+
+	joinParams := make([]interface{}, 0)
+
+	switch joinType {
+	case 0:
+		joinSql = " JOIN "
+	case 1:
+		joinSql = " INNER JOIN "
+	case 2:
+		joinSql = " LEFT JOIN "
+	case 3:
+		joinSql = " RIGHT JOIN "
+	}
+
+	for key := range joinOn {
+		sqlParam := joinOn[key]
+		if sqlParam.Sql != "" {
+			joinSql = joinSql + key + " ON " + sqlParam.Sql
+			joinParams = append(joinParams, sqlParam.Params...)
+		}
+	}
+	q.joinSql = joinSql
+	q.joinParams = joinParams
+	return q
+}
+
+func (q *QuerySQL) Update() *QuerySQL {
 	sql := `UPDATE %s`
 	q.sql = fmt.Sprintf(sql, q.table)
+	q.group = 2
+	return q
 }
 
-func (q *QuerySQL) Delete() {
+func (q *QuerySQL) Delete() *QuerySQL {
 	sql := `DELETE FROM %s`
 	q.sql = fmt.Sprintf(sql, q.table)
+	q.group = 3
+	return q
 }
 
-func (q *QuerySQL) Where(where string, params ...interface{}) {
+func (q *QuerySQL) Where(where string, params ...interface{}) *QuerySQL {
 	q.where = where
 	q.whereParams = params
+	return q
 }
 
-func (q *QuerySQL) Set(set string, params ...interface{}) {
+func (q *QuerySQL) Set(set string, params ...interface{}) *QuerySQL {
 	q.set = set
 	q.setParams = params
+	return q
 }
 
 func (q *QuerySQL) ToSQL() (string, []interface{}) {
 	result := q.sql
 	params := make([]interface{}, 0)
-	if q.set != "" {
-		result = result + " SET " + q.set
-		params = append(params, q.setParams...)
+	if q.group == 4 {
+		result = result + q.joinSql
+		params = append(params, q.joinParams...)
+	}
+	if q.group == 2 {
+		if q.set != "" {
+			result = result + " SET " + q.set
+			params = append(params, q.setParams...)
+		}
 	}
 	if q.where != "" {
 		result = result + " WHERE " + q.where
--- query_test.go
+++ query_test.go
@@ -7,19 +7,23 @@ import (
 )
 
 func TestQuerySQL(t *testing.T) {
-	q := mysqlplus.Query.CreateSQL("tb_dict_info", DictInfo{})
-	q.Select()
-	q.Where("id=? and dict_name like concat('%', ?, '%')", "42087eb009d32bfeaa699e29da379cf4", "es")
-	sql, params := q.ToSQL()
-	fmt.Println(sql)
-	fmt.Println(params...)
+	// q := mysqlplus.Query.CreateSQL("tb_dict_info", DictInfo{}).Select("").Where("id= ? and dict_name like concat('%', ?, '%')", "42087eb009d32bfeaa699e29da379cf4", "es")
+	// sql, params := q.ToSQL()
+	// fmt.Println(sql)
+	// fmt.Println(params...)
 
-	b := mysqlplus.Query.CreateSQL("tb_dict_info", DictInfo{})
-	b.Update()
-	b.Set("dict_name = ?,dict_value = ?", "name", "val")
-	b.Where("id = ?", "42087eb009d32bfeaa699e29da379cf4")
-	sql, params = b.ToSQL()
+	// b := mysqlplus.Query.CreateSQL("tb_dict_info", DictInfo{}).Update().Set("dict_name = ?,dict_value = ?", "name", "val").Where("id = ?", "42087eb009d32bfeaa699e29da379cf4")
+	// sql, params = b.ToSQL()
 
+	// fmt.Println(sql)
+	// fmt.Println(params...)
+
+	c := mysqlplus.Query.CreateSQL("tb_dict_info", DictInfo{}).SelectJoin("a.id, b.id, b.name").Join(1, map[string]mysqlplus.SqlParam{
+		"a": {
+			Sql: "a.id = b.join_id",
+		},
+	}).Where("a.id = ?", "123456")
+	sql, params := c.ToSQL()
 	fmt.Println(sql)
 	fmt.Println(params...)
 }
