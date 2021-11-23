182405d68ec3f8b119c7a6a8f01d33e379743e70
xiaoby <xiaoboyi@allcorehatress.com>
2021-11-23 16:15:43 +0800

    新增QuerySQL.Select方法,基础查询语句初始化

--- query_sql.go
+++ query_sql.go
@@ -1,5 +1,22 @@
 package mysqlplus
 
+import (
+	"fmt"
+	"strings"
+)
+
+type querySQLInterface interface {
+}
+
 type QuerySQL struct {
-	model interface{} // 内核主表模型
+	table  string        // 表名
+	model  interface{}   // 内核主表模型
+	params []interface{} // 实际内核参数
+	sql    string        // 最终sql
+}
+
+func (q *QuerySQL) Select() {
+	c := Util.GetAllTags(q.model, "db")
+	sql := `select %s from %s`
+	q.sql = fmt.Sprintf(sql, strings.Join(c, ","), q.table)
 }
